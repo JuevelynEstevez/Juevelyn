cb50bad05debbd943eb3a4ab6f8c31d7c8e1eb1c
JuevelynEstevez <estevezveras2@gmail.com>
2025-12-08 11:50:39 -0400

    cambio9

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 let listElements = document.querySelectorAll(".list__button--click");
 
 listElements.forEach(listElement => {
-    listElement.addEventListener("click", ()=>{
-         listElement.classList.toggle("arrow");
+    listElement.addEventListener("click", () => {
+        listElement.querySelector(".list__arrow").classList.toggle("list__arrow--active");
 
-    })
+        let menu = listElement.nextElementSibling; 
+        let height = 0;
+
     
-    let height = 0;
-    let menu  = listElement.nextElementSibling;
-    console.log()
-    if(menu.clientHeight == '0'){
-        height= menu.scrollHeight;
-    }
-menu.style.height = '${heigh}px';
+        if (menu.clientHeight === 0) {
+            height = menu.scrollHeight;
+        }
+
+        
+        menu.style.height = `${height}px`;
+    });
 });
+
